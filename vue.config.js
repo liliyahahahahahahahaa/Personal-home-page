e29f42a397f16f765551c1531117f81677cf62e6
liliyahahahahahahahaa <1570403769@qq.com>
2021-07-31 11:38:34 +0800

    product path config

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,10 @@
 const path = require('path')
 
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/workSpace/olivia/dist/'
-    : '../../',
+  publicPath: './',
+  // process.env.NODE_ENV === 'production'
+  //   ? '/workSpace/olivia/dist/'
+  //   : '../../',
   configureWebpack: config => {
     config.resolve.alias = {
       '@': path.resolve(__dirname, 'src')
